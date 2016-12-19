430f23dfe8b72de94f2e56f8bc69e4c56d1a0916
Rold4n <roldanavilam@gmail.com>
2016-12-19 12:53:10 -0400

    crendo proyecto

---
+++ app/src/test/java/umsa/app/roldan/com/tarea1/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package umsa.app.roldan.com.tarea1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
